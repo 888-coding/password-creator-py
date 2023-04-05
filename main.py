8b40da6b354a32a879a70387bc18d652b13fa812
888-coding <pachelee@gmail.com>
2023-04-05 08:46:58 -0300

    Updating the comments for main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# App Password Creator 
+
+print("Welcome to the password creator program")
+
+# This program will ask some question for the user to create
+# the password
+
+# Question 1: 
+# - How many letters
+# Question 2: 
+# - How many simbols
+# Question 3:
+# - How many numbers
+
